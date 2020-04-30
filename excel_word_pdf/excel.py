fd8139b5102a2f5b34e36174af07c46551a497a6
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-04-30 12:22:15 +0530

    getting the cell values from the sheet

--- excel_word_pdf/excel.py
+++ excel_word_pdf/excel.py
@@ -5,5 +5,8 @@ print(type(wb)) #type of the workbook will print
 #knowing the names of the sheet
 sheet_names =next(iter(wb.get_sheet_names()),None) # only gets one sheet name since next is being used
 print(sheet_names)
-sheet = wb.get_sheet_by_name(sheet_names) # geting he sheet object 
-
+sheet = wb.get_sheet_by_name(sheet_names) # geting the sheet object
+print(sheet['A1'].value)
+#geting the the cell object using cell() method
+cell = sheet.cell(row=1,column=1)
+print(cell.value)
