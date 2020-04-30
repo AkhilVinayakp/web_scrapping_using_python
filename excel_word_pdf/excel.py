a70a68ae8791beed2f1d3f65bc17d46930c2586c
AkhilVinayakp <nirvanaosborn@gmail.com>
2020-04-30 12:19:05 +0530

    getting the sheet object

--- excel_word_pdf/excel.py
+++ excel_word_pdf/excel.py
@@ -1,1 +1,9 @@
+import openpyxl as xl
+# opening the workbook via load_workbook() method
+wb = xl.load_workbook('example.xlsx')
+print(type(wb)) #type of the workbook will print
+#knowing the names of the sheet
+sheet_names =next(iter(wb.get_sheet_names()),None) # only gets one sheet name since next is being used
+print(sheet_names)
+sheet = wb.get_sheet_by_name(sheet_names) # geting he sheet object 
 
